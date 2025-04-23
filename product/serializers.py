1fd5c13c3609a664b23e6ff4a13c9e3b307d2474
BeknazarBolotov <bolotov.beknazar08@gmail.com>
2025-04-23 18:41:43 +0600

    HW4

--- product/serializers.py
+++ product/serializers.py
@@ -12,16 +12,44 @@ class CategorySerializer(serializers.ModelSerializer):
     def get_products_count(self, obj):
         return Category.objects.all().count()
 
+    def validate_name(self, value):
+        if len(value.strip()) < 2:
+            raise serializers.ValidationError("Название категории должно быть длиннее 1 символа.")
+        return value
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+        def validate_text(self, value):
+            if not value.strip():
+                raise serializers.ValidationError("Текст отзыва не может быть пустым")
+            if len(value) > 2000:
+                raise serializers.ValidationError("Текст отзыва слишком длинный (максимум 2000 символов)")
+            return value
+
+        def validate_rating(self, value):
+            if value < 1 or value > 5:
+                raise serializers.ValidationError("Рейтинг должен быть от 1 до 5")
+            return value
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Название товара не может быть пустым.")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть положительной.")
+        return value
 
 class ProductWithReviewsSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True)
@@ -34,6 +62,17 @@ class ProductWithReviewsSerializer(serializers.ModelSerializer):
     def get_average_rating(self, obj):
         avg_rating = obj.reviews.aggregate(Avg('stars'))['stars__avg']
         return round(avg_rating or 0, 2)
+
+
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Рейтинг должен быть от 1 до 5.")
+        return value
+
+    def validate_text(self, value):
+        if len(value.strip()) < 10:
+            raise serializers.ValidationError("Текст отзыва должен содержать минимум 10 символов.")
+        return value
 
 
 
